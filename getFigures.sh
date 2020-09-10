dd0331402e786e195011f009573e5b2b94dba35a
Michael Hahn <mhahn29@gmail.com>
2020-09-10 09:02:25 +0800

    code

---
+++ getFigures.sh
@@ -0,0 +1,5 @@
+cp ~/morpheme-ordering/utils/turkish/visualize/Turkish-suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/
+cp ~/morpheme-ordering/utils/turkish/visualize/Turkish-suffixes-byMorphemes-auc-hist-heldout-Nouns-Coarse-FineSurprisal-optimized.pdf figures/
+cp ~/morpheme-ordering/utils/visualize/Hungarian-suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/
+cp ~/morpheme-ordering/utils/visualize/Hungarian-suffixes-byMorphemes-auc-hist-heldout-Nouns-Coarse-FineSurprisal-optimized.pdf figures/
+
