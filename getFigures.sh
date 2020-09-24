b54d68681560bce0cef83203b23ae054ffe13f44
Michael Hahn <mhahn29@gmail.com>
2020-09-24 10:11:48 +0800

    comparisons

--- getFigures.sh
+++ getFigures.sh
@@ -9,8 +9,18 @@ cp ~/morpheme-ordering/utils/turkish_verbs/analyze/suffixes-byMorphemes-auc-hist
 cp ~/morpheme-ordering/utils/hungarian_verbs/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/hungarian_verbs/
 cp ~/morpheme-ordering/utils/finnish_verbs/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/finnish_verbs/
 cp ~/morpheme-ordering/utils/korean/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/korean/
-cp ~/morpheme-ordering/utils/turkish_michael/visualize/comparison.tex figures/turkish_verbs/
 cp ~/morpheme-ordering/utils/japanese/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/japanese/
 cp ~/morpheme-ordering/utils/sesotho_prefixes/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/sesotho_prefixes/
 cp ~/morpheme-ordering/utils/sesotho_suffixes/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/sesotho_suffixes/
+
+cp ~/morpheme-ordering/utils/turkish_verbs/visualize/comparison.tex figures/turkish_verbs/
+cp ~/morpheme-ordering/utils/turkish_nouns/visualize/comparison.tex figures/turkish_nouns/
+cp ~/morpheme-ordering/utils/finnish_verbs/visualize/comparison.tex figures/finnish_verbs/
+cp ~/morpheme-ordering/utils/finnish_nouns/visualize/comparison.tex figures/finnish_nouns/
+cp ~/morpheme-ordering/utils/hungarian_verbs/visualize/comparison.tex figures/hungarian_verbs/
+cp ~/morpheme-ordering/utils/hungarian_nouns/visualize/comparison.tex figures/hungarian_nouns/
+cp ~/morpheme-ordering/utils/korean/visualize/comparison.tex figures/korean/
+cp ~/morpheme-ordering/utils/japanese/visualize/comparison.tex figures/japanese/
+cp ~/morpheme-ordering/utils/sesotho_prefixes/visualize/comparison.tex figures/sesotho_prefixes/
+cp ~/morpheme-ordering/utils/sesotho_suffixes/visualize/comparison.tex figures/sesotho_suffixes/
 
