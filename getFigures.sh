2bdbbbda11cd61ac8e17dc941af943a1ca683cc7
Michael Hahn <mhahn29@gmail.com>
2021-07-01 17:38:04 +0800

    code

--- getFigures.sh
+++ getFigures.sh
@@ -4,10 +4,10 @@ cp ../morpheme-ordering/utils/visualize/Hungarian-suffixes-byMorphemes-auc-hist-
 cp ../morpheme-ordering/utils/visualize/Hungarian-suffixes-byMorphemes-auc-hist-heldout-Nouns-Coarse-FineSurprisal-optimized.pdf figures/
 cp ../morpheme-ordering/utils/turkish_nouns_adj/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/turkish_nouns/
 cp ../morpheme-ordering/utils/hungarian_nouns/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/hungarian_nouns/
-cp ../morpheme-ordering/utils/finnish_nouns_adj/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/finnish_nouns/
+cp ../morpheme-ordering/utils/finnish_nouns_adj_joint/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/finnish_nouns/
 cp ../morpheme-ordering/utils/turkish_verbs/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/turkish_verbs/
 cp ../morpheme-ordering/utils/hungarian_verbs/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/hungarian_verbs/
-cp ../morpheme-ordering/utils/finnish_verbs/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/finnish_verbs/
+cp ../morpheme-ordering/utils/finnish_verbs_joint/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/finnish_verbs/
 cp ../morpheme-ordering/utils/korean/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/korean/
 cp ../morpheme-ordering/utils/japanese/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/japanese/
 cp ../morpheme-ordering/utils/sesotho_prefixes/analyze/suffixes-byMorphemes-auc-hist-heldout-Coarse-FineSurprisal-optimized.pdf figures/sesotho_prefixes/
@@ -15,8 +15,8 @@ cp ../morpheme-ordering/utils/sesotho_suffixes/analyze/suffixes-byMorphemes-auc-
 
 cp ../morpheme-ordering/utils/turkish_verbs/visualize/comparison-opt-uni.tex figures/turkish_verbs/
 cp ../morpheme-ordering/utils/turkish_nouns_adj/visualize/comparison-opt-uni.tex figures/turkish_nouns/
-cp ../morpheme-ordering/utils/finnish_verbs/visualize/comparison-opt-uni.tex figures/finnish_verbs/
-cp ../morpheme-ordering/utils/finnish_nouns_adj/visualize/comparison-opt-uni.tex figures/finnish_nouns/
+cp ../morpheme-ordering/utils/finnish_verbs_joint/visualize/comparison-opt-uni.tex figures/finnish_verbs/
+cp ../morpheme-ordering/utils/finnish_nouns_adj_joint/visualize/comparison-opt-uni.tex figures/finnish_nouns/
 cp ../morpheme-ordering/utils/hungarian_verbs/visualize/comparison-opt-uni.tex figures/hungarian_verbs/
 cp ../morpheme-ordering/utils/hungarian_nouns/visualize/comparison-opt-uni.tex figures/hungarian_nouns/
 cp ../morpheme-ordering/utils/korean/visualize/comparison-opt-uni.tex figures/korean/
@@ -27,8 +27,8 @@ cp ../morpheme-ordering/utils/sesotho_suffixes/visualize/comparison-opt-uni.tex 
 
 cp ../morpheme-ordering/utils/turkish_verbs/visualize/comparison.tex figures/turkish_verbs/
 cp ../morpheme-ordering/utils/turkish_nouns_adj/visualize/comparison.tex figures/turkish_nouns/
-cp ../morpheme-ordering/utils/finnish_verbs/visualize/comparison.tex figures/finnish_verbs/
-cp ../morpheme-ordering/utils/finnish_nouns_adj/visualize/comparison.tex figures/finnish_nouns/
+cp ../morpheme-ordering/utils/finnish_verbs_joint/visualize/comparison.tex figures/finnish_verbs/
+cp ../morpheme-ordering/utils/finnish_nouns_adj_joint/visualize/comparison.tex figures/finnish_nouns/
 cp ../morpheme-ordering/utils/hungarian_verbs/visualize/comparison.tex figures/hungarian_verbs/
 cp ../morpheme-ordering/utils/hungarian_nouns/visualize/comparison.tex figures/hungarian_nouns/
 cp ../morpheme-ordering/utils/korean/visualize/comparison.tex figures/korean/
@@ -39,4 +39,5 @@ cp ../morpheme-ordering/utils/output/accuracies.tex figures/
 cp ../morpheme-ordering/utils/output/accuracies*pdf figures/
 
 
-cp ../morpheme-ordering/utils/visualize/visualizeCPMI.R_*.pdf figures/
+cp ../morpheme-ordering/utils/visualize/visualizeCPMI.R_nouns.pdf figures/visualizeCPMI_R_nouns.pdf
+cp ../morpheme-ordering/utils/visualize/visualizeCPMI.R_nouns.pdf figures/visualizeCPMI_R_nouns.pdf
